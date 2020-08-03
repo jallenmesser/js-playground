613614dbc58b3937a4ac9d350cb968acc9a1f1a8
jallenmesser <jallenmessersmith@gmail.com>
2020-08-02 20:53:16 -0400

    background color changing button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-console.log('Hello Project.');
-let button = document.createElement('button')
-document.append(button)
+    let page = document.getElementsByTagName('body')[0]
+    let button = document.createElement('button')
+    button.innerHTML = 'I change the background color!'
+    page.append(button)
+
+    const setBg = () => {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        page.style.backgroundColor = "#" + randomColor;
+      }
+      
+      button.addEventListener("click", setBg);
+      setBg();
 
 })

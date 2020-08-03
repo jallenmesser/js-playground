552146988e92fd27363fef4e03a19534f4aa7661
jallenmesser <jallenmessersmith@gmail.com>
2020-08-03 13:23:10 -0400

    Added more features to background color

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
     let page = document.getElementsByTagName('body')[0]
+
+    let backgroundColorChangingDiv = document.createElement('div')
+    backgroundColorChangingDiv.className = "color-changing"
+    page.append(backgroundColorChangingDiv)
+
+    let colorCode = document.createElement('h4')
+    let backgroundColorTitle = document.createElement('h3')
+    backgroundColorTitle.innerHTML = "The background is currently"
+    backgroundColorChangingDiv.append(backgroundColorTitle,colorCode)
+
     let button = document.createElement('button')
-    button.innerHTML = 'I change the background color!'
-    page.append(button)
+    button.className = 'button'
+    button.innerHTML = 'Change the background color!'
+    backgroundColorChangingDiv.append(button)
 
     const setBg = () => {
         const randomColor = Math.floor(Math.random()*16777215).toString(16);
         page.style.backgroundColor = "#" + randomColor;
+        colorCode.innerHTML = '#' + randomColor
       }
       
       button.addEventListener("click", setBg);
